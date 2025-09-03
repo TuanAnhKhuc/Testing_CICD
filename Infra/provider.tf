2180506d3f1220a577507fbe07e6309fc899ce20
AnhKT4 <anhkt4@fpt.com>
2025-09-03 13:19:54 +0700

    Setting up basic file struc

---
+++ Infra/provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region  = "ap-southeast-1"
+  profile = "anhkt4"
+}
+
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
